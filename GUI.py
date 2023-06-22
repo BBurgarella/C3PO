99087a6935babbae6ce3b57af549ed1ce6b0f43a
Boris Burgarella <boris@macbook-air-de-boris.home>
2023-06-22 23:58:00 +0200

    included minor changes for maxOS compatibility 23:58  up 19 mins, 2 users, load averages: 1,56 2,51 3,93 USER     TTY      FROM              LOGIN@  IDLE WHAT boris    console  -                23:43      15 - boris    s000     -                23:51       7 -zsh          ÿ¿    /bin/zsh

--- GUI.py
+++ GUI.py
@@ -42,21 +42,21 @@ class MainApp(App):
 
         for file in files:
             if ".json" in file:
-                btn = Button(text=file[:-5], size_hint_y=None, height=44)
+                btn = Button(text=file[:-5], size_hint_y=None, size_hint_x=0.3, height=44, width=150)
                 btn.bind(on_release=lambda btn=btn: dropdown.select(btn.text))
                 dropdown.add_widget(btn)
         # Create the main button
-        main_button = Button(text='Choose an agent', size_hint=(None, None), height=44, width=150)
+        main_button = Button(text='Choose an agent', size_hint=(0.5, 0.5), height=44, width=300)
 
         self.languages = {'Afrikaans': 'af', 'Arabic': 'ar', 'Bulgarian': 'bg', 'Bengali': 'bn', 'Bosnian': 'bs', 'Catalan': 'ca', 'Czech': 'cs', 'Danish': 'da', 'German': 'de', 'Greek': 'el', 'English': 'en', 'Spanish': 'es', 'Estonian': 'et', 'Finnish': 'fi', 'French': 'fr', 'Gujarati': 'gu', 'Hindi': 'hi', 'Croatian': 'hr', 'Hungarian': 'hu', 'Indonesian': 'id', 'Icelandic': 'is', 'Italian': 'it', 'Hebrew': 'iw', 'Japanese': 'ja', 'Javanese': 'jw', 'Khmer': 'km', 'Kannada': 'kn', 'Korean': 'ko', 'Latin': 'la', 'Latvian': 'lv', 'Malayalam': 'ml', 'Marathi': 'mr', 'Malay': 'ms', 'Myanmar (Burmese)': 'my', 'Nepali': 'ne', 'Dutch': 'nl', 'Norwegian': 'no', 'Polish': 'pl', 'Portuguese': 'pt', 'Romanian': 'ro', 'Russian': 'ru', 'Sinhala': 'si', 'Slovak': 'sk', 'Albanian': 'sq', 'Serbian': 'sr', 'Sundanese': 'su', 'Swedish': 'sv', 'Swahili': 'sw', 'Tamil': 'ta', 'Telugu': 'te', 'Thai': 'th', 'Filipino': 'tl', 'Turkish': 'tr', 'Ukrainian': 'uk', 'Urdu': 'ur', 'Vietnamese': 'vi', 'Chinese (Simplified)': 'zh-CN', 'Chinese (Mandarin/Taiwan)': 'zh-TW', 'Chinese (Mandarin)': 'zh'}
         for language, key in self.languages.items():
-            btn = Button(text=language, size_hint_y=None, height=44)
+            btn = Button(text=language, size_hint_y=None, height=44, width=150)
             btn.bind(on_release=lambda btn=btn: lang_drop.select(btn.text))
             lang_drop.add_widget(btn)
 
 
         # Create the main button
-        main_button_lang = Button(text='Choose a language', size_hint=(None, None), height=44, width=150)
+        main_button_lang = Button(text='Choose a language', size_hint=(0.5, 0.5), height=44, width=300)
 
         # Show the dropdown menu when the main button is released
         # Note: all the bind() calls pass the instance of the caller (here, the button),
